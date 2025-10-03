a470241e80e8aa5e1e5bd98a8e76b3d5248d5e3b
D32\zak <spseplzen@spseplzen.cz>
2025-10-03 12:00:43 +0200

    commit

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ print(b) # komentář
 # **************
 # dulezita cast kodu
 # ***************
+
+#datové typy v pythonu
+
+samopopisne_nazvy_promennych = "furryboy2011"
+print(samopopisne_nazvy_promennych)
+print(type(samopopisne_nazvy_promennych))
+
+cislo = 9
+print(cislo)
+print(type(cislo))
+
+desetinne_cislo = 5.5
+print(desetinne_cislo)
+print(type(desetinne_cislo))
