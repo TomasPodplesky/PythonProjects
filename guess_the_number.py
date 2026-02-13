50cad972ef3d38ee44f4cc94269a2e25207f07a0
D32\zak <spseplzen@spseplzen.cz>
2026-02-13 11:19:46 +0100

    commit

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,14 +1,20 @@
 import numpy as np
 
 cislo_hadane = np.random.randint(1,20)
-
+pokusy = 0
 while(True):
     cislo_x = input("hadej cislo: ")
-    cislo = int(cislo_x)
-    if(cislo == cislo_hadane):
-        print("spráně!!")
-        break
-    elif(cislo<cislo_hadane):
-        print("špatně, hádej vyšší")
-    elif(cislo>cislo_hadane):
-        print("špatně, hádej nížš")
+    try:
+        cislo = int(cislo_x)
+        if(cislo == cislo_hadane):
+            print("spráně!!")
+            print("travlo to pokusů: " + str(pokusy))
+            break
+        elif(cislo<cislo_hadane):
+            print("špatně, hádej vyšší")
+            pokusy += 1
+        elif(cislo>cislo_hadane):
+            print("špatně, hádej nížš")
+        pokusy += 1
+    except:
+        print("nezadal jsi číslo")
