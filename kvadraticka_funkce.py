53a90fb831c20f047c080388b880053f1f9907e5
D32\zak <spseplzen@spseplzen.cz>
2026-02-20 11:22:57 +0100

    commit

--- kvadraticka_funkce.py
+++ kvadraticka_funkce.py
@@ -4,12 +4,28 @@ def kvadr(a,b,c):
     if(D>0):
         x1 = (-b+pow(D,1/2))/(2*a)
         x2 = (-b-pow(D,1/2))/(2*a)
-        vysledek = print("řešení jsou, x1: " + str(x1) + " x2: " + str(x2))
+        vysledek_text = "řešení jsou, x1: " + str(x1) + " x2: " + str(x2)
+        print(vysledek_text)
+        vysledek = [x1, x2]
     elif(D<0):
         x1 = (-b+pow(D,2))/2*a
-        vysledek = print("řešení je: " +str(x1))
+        vysledek_text = "řešení je: " +str(x1)
+        print(vysledek_text)
+        vysledek = [x1]
     elif(D==0):
-        vysledek = print("nemá řešení")
+        vysledek_text = "nemá řešení"
+        print(vysledek_text)
+        vysledek = []
     return vysledek
 
-kvadr(1,3,1)
+#kvadr(1,3,1)
+print("*************************")
+def kvadr_test():
+    results=[]
+    for i in range(5):
+        results.append(kvadr(1,i,1))
+    return results
+
+vysledky = kvadr_test()
+for vysledky in vysledky:
+    print(vysledky)
